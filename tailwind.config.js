725c2aa061a25040d4a25c42c83ce0d43362d763
Taro Morita <morita0079@gmail.com>
2023-02-15 21:14:44 +0900

    change

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,12 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    "./app/**/*.{js,ts,tsx,jsx}",
-    "./pages/**/*.{js,ts,tsx,jsx}",
-    "./components/**/*.{js,ts,tsx,jsx}"
+    "./src/**/*.{js,ts,tsx,jsx}",
+    "./src/pages/**/*.{js,ts,tsx,jsx}",
+    "./src/components/**/*.{js,ts,tsx,jsx}",
   ],
   theme: {
     extend: {},
   },
   plugins: [],
-}
+};

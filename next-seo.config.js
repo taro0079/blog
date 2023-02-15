49f845d6d1278fdf7bd0775ed1e4b943b5cfbd49
Taro Morita <morita0079@gmail.com>
2023-02-15 23:17:07 +0900

    SEOの設定

---
+++ next-seo.config.js
@@ -0,0 +1,15 @@
+export default {
+  title: "My new title",
+  description: "some",
+  openGraph: {
+    type: "website",
+    locale: "ja_JP",
+    url: "https://example.com/",
+    site_name: "BLOG",
+  },
+  twitter: {
+    handle: "@taro",
+    site: "@site",
+    cardType: "summary_large_image",
+  },
+};
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,10 +1,13 @@
 import "src/styles/globals.css";
 import type { AppProps } from "next/app";
 import Layout from "./components/Layout";
+import SEO from "next-seo.config";
+import { DefaultSeo } from "next-seo";
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <Layout>
+      <DefaultSeo {...SEO} />
       <Component {...pageProps} />
     </Layout>
   );
